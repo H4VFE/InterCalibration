51b3a9a8aa0ecabd5822b1f1c556db8ed4293223
Kelsey Amber Yee <kyee@lxplus089.cern.ch>
2017-07-27 14:11:44 +0200

    Working on MyAnalysis

--- WeightedX/MyAnalysis.C
+++ WeightedX/MyAnalysis.C
@@ -60,10 +60,29 @@ void MyAnalysis::Loop()
       posE.push_back(make_pair(4.4, maximum[VFE3_2]/3480));
       posE.push_back(make_pair(6.6, maximum[VFE5_2]/3400));
       posE.push_back(make_pair(8.8, maximum[VFE4_2]/3930));
+      
+      float energy_cluster = 0;
+      float _w0 = 3.8;
+      float x_cluster_logE = 0;
 
+      for(vector<pair<float, float> >::iterator ii = posE.begin(); ii != posE.end(); ii++) {
+	//   std::cout << " energy = " << - ii->first << std::endl;
+	  float energy_temp = - ii->second;
+	  energy_cluster = energy_cluster + energy_temp;
+      }
 
-      //      for(vector<pair<float, float> >::iterator it = posE.begin(); it != posE.end(); it++)                                                                                                        
-      //    	cout << "(" << it->first << ", " << it->second << ")" << endl;
+      for(vector<pair<float, float> >::iterator ii = posE.begin(); ii != posE.end(); ii++)
+	{                                                                                                       
+	//    	cout << "(" << ii->first << ", " << ii->second << ")" << endl;
+	  float energy_temp = - ii->second; 
+	  float wi = (_w0 + log(energy_temp/energy_cluster));
+	  if (wi > 0)
+	  {
+	    x_cluster_logE = x_cluster_logE + (ii->first) * wi ;
+	  }
+
+	}
+
 
 
       // Energy Plots                                                                                                                                                                               
@@ -74,74 +93,6 @@ void MyAnalysis::Loop()
           hprof->Fill(Y[1]/2,maximum[VFE3_2]);                                                                                                                                                             
         }                                                                                                                                                                                                 
       */                                                                                                                                                                                                
-
-
-
-  /*    
-      // Andrea's Code Part
-      int  num_clusters = 0;
-      float _w0 = 3.8;
-      float x_cluster_logE = 0;
-      float y_cluster_logE = 0;
-      float weight_cluster = 0;
-      float energy_cluster = 0;
-      float x_cluster_max = -1000;
-      float y_cluster_max = -1000;
-      std::map < float, std::pair<float, float> > map_of_calo_clusters;
-
-      //---- first calculate cluster energy
-      for( std::map < float, std::pair<float, float> >::iterator ii=map_of_calo_clusters.begin(); ii!=map_of_calo_clusters.end(); ii++) {
-	//   std::cout << " energy = " << - ii->first << std::endl;
-	if (x_cluster_max == -1000) {
-	  x_cluster_max = (ii->second.first); //---- seed position X
-	  y_cluster_max = (ii->second.second); //---- seed position X   
-	  float energy_temp = - ii->first;
-	  energy_cluster = energy_cluster + energy_temp;
-	  _vector_energies.push_back(energy_temp);
-	  num_clusters++;
-	}
-	else {
-	  if (DR(ii->second.first, x_cluster_max, ii->second.second, y_cluster_max) < maxDR) {
-	    float energy_temp = - ii->first;
-	    energy_cluster = energy_cluster + energy_temp;
-	    _vector_energies.push_back(energy_temp);
-	    num_clusters++;
-	  }
-	}
-      }
-
-
-      // Highlighted Code
-      num_clusters = 0;
-
-      if (energy_cluster > 0)
-	{
-	  for( std::map < float, std::pair<float, float> >::iterator ii = map_of_calo_clusters.begin(); ii != map_of_calo_clusters.end(); ii++)
-	    {
-	      //   std::cout << " energy = " << - ii->first << std::endl;                                                                                                                   
-	      //   One task: Find out the equivalent to ii->first                        
-	      if (DR(ii->second.first, x_cluster_max, ii->second.second, y_cluster_max) < maxDR)
-		{
-		  float energy_temp = - ii->first;
-		  // Find the equivalent to ii->first;
-		  float wi = (_w0 + log(energy_temp/energy_cluster));
-		  // energy_cluster = energy_cluster + energy_temp;
-		  if (wi > 0)
-		    {
-		      x_cluster_logE = x_cluster_logE + (ii->second.first)  * wi ;
-		      // x_cluster_logE starts at 0, and then is increased as such. 
-		      weight_cluster = weight_cluster + wi;
-		      // starts at zero and is increased like this. 
-		      
-		      num_clusters++;
-		      // Just a counter
-		    }
-
-
-		}
-	    }
-	}
-*/
 
 
       // if (Cut(ientry) < 0) continue; Put cuts etc...
