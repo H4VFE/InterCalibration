740e472093a863ac3ebb46d27bc7702d740ef4ed
Kelsey Amber Yee <kyee@lxplus026.cern.ch>
2017-08-18 14:58:18 +0200

    Code is cleaned up and a few little things fixed.

--- TemplateFit/Analysis.C
+++ TemplateFit/Analysis.C
@@ -63,8 +63,6 @@ float ymin = -6;
 float ymax = 4;
 
 
-//string energy = "20";
-
 ROOT::Math::Interpolator inter(nsamples+1, ROOT::Math::Interpolation::kCSPLINE);
 double fit_function(double *v,double *par)
 {
@@ -114,8 +112,7 @@ void Analysis::Loop()
    hodoTree->SetBranchAddress("run",&run);
    hodoTree->SetBranchAddress("spill",&spill);
    hodoTree->SetBranchAddress("event",&event);
-   //  TFile *WCfile = new TFile("/afs/cern.ch/work/a/ajofrehe/cern-summer-2016/July2017/H4Analysis/ntuples/vfe_55_WC_B3_100.root"); //Same issue as before                                                  
-   //  TFile *WCfile = new TFile("/afs/cern.ch/user/k/kyee/work/vfe_ecal_tia_7277.root");                                                                                                                    
+  
    TTree *WCtree = (TTree*) WCfile->Get("h4");
    WCtree->SetBranchAddress("X",WCX);
    WCtree->SetBranchAddress("Y",WCY);
@@ -135,21 +132,17 @@ void Analysis::Loop()
    template_tree->Branch("run",&Run,"run/i");
    template_tree->Branch("spill",&Spill,"spill/i");
    template_tree->Branch("event",&Event,"event/i");
-   //TH1F *amp = new TH1F("amplitude obtained by templates","amplitude obtained by templates",100,0,5000);
    TH1F *template_time = new TH1F("time obtained by templates","time obtained by templates",120,-30,30);
    TH1F*plot = new TH1F("plot", "3x3 Energy Sum", 100, rbot, rtop);
    TH1F*plot2 = new TH1F("plot2", "Second plot", 100, rbot, rtop);
-   //   plot->GetXaxis()->SetRange(1, 3);
 
    TProfile *XwtoE = new TProfile("XwtoE","Energy", 100, -50, 50, 300, 6000); //
    XwtoE->GetXaxis()->SetTitle("Xw");
    XwtoE->GetYaxis()->SetTitle("Energy");
-   //Weighted X to energy                                                                                                                                                                                   
-
+ 
    TProfile *YwtoE = new TProfile("YwtoE","Energy", 100, -50, 50, 300, 6000); //                                                                                                                          
    YwtoE->GetXaxis()->SetTitle("Yw");
    YwtoE->GetYaxis()->SetTitle("Energy");
-   //Weighted Y to energy                                                                                                                                                                                 
 
    TH2F*both = new TH2F("both","both", 100, 0, 4000*scale, 100, 0, 4000*scale );  
 
@@ -171,15 +164,6 @@ void Analysis::Loop()
    func->SetParLimits(0,30,20000);
    func->SetParLimits(1,-30,30);
    func->SetParLimits(2,0.95,1.1);
-
-   //float channel_peak[25] = {2790,3640,3480,3970,3770,3270,3280,3930,4410,4270,3390,3810,3400,3780,2850,4260,4260,3700,3730,3720,4060,3860,3600,4260,4290};                                              
-
-   /*  
-   float channel_peak[25] = {2850,3740,3620,4044,3940,3320,3400,4010,4590,4530,3510,3970,3520,3800,2980,4310,4260,3740,3790,3750,4160,4020,3650,4330,4400};
-   float channel_x[25] = {0,0,0,0,0,2,2,2,2,2,1,1,1,1,1,-1,-1,-1,-1,-1,-2,-2,-2,-2,-2};
-   float channel_y[25] = {2,1,0,-1,-2,2,1,0,-1,-2,-2,-1,0,1,2,-2,-1,0,1,2,2,1,0,-1,-2};
-   */
-   //Here are the calibrations. Note: the positions are done likely, with C in the center and with "crystal distances" as units.                                                                          
   
    float energy_sum;
    float position_weight[25];
@@ -211,43 +195,7 @@ void Analysis::Loop()
      Run = run;
      Spill = spill;
      Event = event;
-
-     /*
-     for(int channel = 0;channel < 25;channel++){
-       for (int i = 0;i < nsamples;i++) waveform->SetBinContent(i+1,WF_val[i+channel*nsamples]);
-       max_time[channel] = ( 6.25 * waveform->GetMaximumBin() ) - 3.25;
-       digiMax[channel] = waveform->GetMaximum();
-       if (waveform->GetMaximum() < 30){
-	 temp_amp[channel] = 1;
-	 temp_time[channel] = -10000;
-       }else{
-	 func->SetParameters(1.2*waveform->GetMaximum(),0,1);
-	 //waveform->Fit("fit","Q","",200,380);
-	 waveform->Fit("fit","Q","",269.1+func->GetParameter(1)-20,269.1+func->GetParameter(1)+30);                                                                                                     
- 	 //if (jentry == 44 && channel == 17) break;                                                                                                                                                      
-  	 //template_time->Fill(func->GetParameter(1));                                                                                                                                                     
-	 temp_amp[channel] = func->GetParameter(0);
-	 temp_time[channel] = func->GetParameter(1);
-       }
-     }
-     */
-     /*
-     energy_sum = 0;
-     position_weight_sum = 0;
-
-     for (int i = 0;i < 25;i++) energy_sum += temp_amp[i]/channel_peak[i];
-     for(int channel = 0;channel < 25;channel++){
-       position_weight[channel] = 3 + TMath::Log10(temp_amp[channel]/(channel_peak[channel]*energy_sum));
-       if (position_weight[channel] < 0) position_weight[channel] = 0;
-       position_weight_sum += position_weight[channel];
-     }
-     EA_X = 0;
-     for(int channel = 0;channel < 25;channel++ && position_weight_sum != 0) EA_X += 22.0 * channel_x[channel] * position_weight[channel]/position_weight_sum;
-     EA_Y = 0;
-     for(int channel = 0;channel < 25;channel++ && position_weight_sum != 0) EA_Y += 22.0 * channel_y[channel] * position_weight[channel]/position_weight_sum;
-     //    cout << EA_Y << endl;                                                                                                                                                                          
-     */  
-  
+ 
      template_tree->Fill();
 
      for(int channel = 0;channel < 25;channel++){
@@ -259,24 +207,14 @@ void Analysis::Loop()
 	 temp_time[channel] = -10000;
        }else{
 	 func->SetParameters(1.2*waveform->GetMaximum(),0,1);
-	 // waveform->Fit("fit","Q","",200,380);
 	 waveform->Fit("fit","Q","",269.1+func->GetParameter(1)-20,269.1+func->GetParameter(1)+30);                                                                                                     
-	 //if (jentry == 44 && channel == 17) break;                                                                                                                                                       
 
-	 //	 cout << EA_X << " " <<  func->GetParameter(0) << endl ; 
-	 /*
-	 XwtoE->Fill(EA_X , func->GetParameter(0));
-	 YwtoE->Fill(EA_Y , func->GetParameter(0));
-	 */
-
-	 //     	if (EA_X > -6   && EA_X < 1 && EA_Y > -4  && EA_Y < 3 && func->GetParameter(0) > coff )
 	 ientry = LoadTree(jentry);
 	 if (ientry < 0) break;
 	 nb = fChain->GetEntry(jentry);
 	
 	 std::map<std::pair<int, int>, float> mapE;
-
-	 //New mapping                                                                                                                                                                                         
+    
 	 mapE.insert(make_pair(make_pair(-44, 44), maximum[VFE8_0]/4160));
 	 mapE.insert(make_pair(make_pair(-22, 44), maximum[VFE7_4]/3750));
 	 mapE.insert(make_pair(make_pair(0, 44), maximum[VFE3_0]/2850));
@@ -316,19 +254,19 @@ void Analysis::Loop()
 	 float weight_cluster = 0;
 	 float wi = 0;
 
-	 if( maximum[num] > 2000 )  // Necessary to cut the pions out
+	 if( maximum[num] > coff )  // Necessary to cut the pions out
 	   {
 	     for(std::map <  std::pair<int, int>, float >::iterator ii = mapE.begin(); ii != mapE.end(); ii++)
 	       {
 		 float energy_temp =  ii->second;
-		 if( energy_temp > 1000/3000) energy_cluster = energy_cluster + energy_temp;
+		 if( energy_temp > 0.5*coff/3000) energy_cluster = energy_cluster + energy_temp;
 	       }
 
 
 
 	     for(std::map <  std::pair<int, int>, float >::iterator ii = mapE.begin(); ii != mapE.end(); ii++)
 	       {		 float energy_temp =  ii->second;
-		 if( energy_temp > 1000/3400 )
+		 if( energy_temp > 0.5*coff/3400 )
 		   {
 		     wi = (_w0 + log(energy_temp/energy_cluster));
 		     if (wi > 0)
@@ -365,29 +303,17 @@ void Analysis::Loop()
 	     D3 = 0;
 	     D4 = 0;
 
-	     //amp->Fill(func->GetParameter(0))	   
 	       ientry = LoadTree(jentry);
 	       if (ientry < 0) break;
 	       nb = fChain->GetEntry(jentry);
 	       //NOTE: Anything having to do with the maximum, VFE#_#, etc... has to be put after something like this. 
 	      
-	       /*
-	       cout << VFE3_2 <<  "test VFE3_2" << endl;                                                                                                                                                  
-    	       cout << maximum[VFE3_2] << "test maximum[VFE3_2]" <<endl; 
-	       */ //Check successful
-
 	     //From here, I'll try channel plugging. 
 	     if(channel == 2)
 	       {
 		 C3 = func->GetParameter(0)/3640;
 		 plot2->Fill(C3);
 		 both->Fill(func->GetParameter(0),maximum[VFE3_2]);
-	       // plot->Fill(func->GetParameter(0)); 
-	       // cout << "Template Amplitude C3 " << func->GetParameter(0)/3640 << endl; 
-	       /* //Check seems reasonable. 
-	       cout << "Template Amplitude " << func->GetParameter(0)/3640 << endl;
-	       cout << "Maximum scaled " << maximum[VFE3_2]/3640 << endl;
-	       */
 	       }
 	    
              if(channel == 18)
@@ -435,26 +361,9 @@ void Analysis::Loop()
 	   }
 	   }
        }
-	 //template_time->Fill(func->GetParameter(1));                                                                                                                                                     
 
      }
    } //Wihin thhe long jentry thing
-
-
-    /*
-       for (Long64_t jentry=0; jentry<nentries;jentry++) {
-	 Long64_t ientry = LoadTree(jentry);
-	 if (ientry < 0) break;
-	 nb = fChain->GetEntry(jentry);   nbytes += nb;
-
-      
-     //cout << VFE3_2 <<  "test VFE3_2" << endl;
-     //cout << maximum[VFE3_2] << "test maximum[VFE3_2]" <<endl;
-      // Check successful. maximum and VFE#_# can be "summoned" here. 
-
-       plot->Fill(maximum[VFE3_2]);
-     }
-   */
 
 
    template_recos->cd();
